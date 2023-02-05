920b28b8390bf589fa4e7c9681e49a0583c4425c
anadreth <miroslav13vano@gmail.com>
2023-02-05 19:18:33 +0100

    Optimising .1

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -35,11 +35,11 @@ const store = configureStore({
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <>
+  <React.StrictMode>
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistStore(store)}>
         <App />
       </PersistGate>
     </Provider>   
-  </>
+  </React.StrictMode>
 )
